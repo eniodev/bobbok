690cf280d39d3c22235d3ce0b349cbf2914a76f0
Énio Carlos Paulo <eniocarlospaulo@MBP-de-Enio.Home>
2026-06-23 23:15:06 +0100

    fix: header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://eniocarlospaulo.github.io',
   base: '/bobbok',
+  trailingSlash: 'always',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
